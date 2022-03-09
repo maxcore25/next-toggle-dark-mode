3ba349bd1bd8d31940f9ae78258b28d7960e6cf1
maxcore25 <mkorobov2002@mail.ru>
2022-03-09 15:41:04 +0300

    _document.js Created And Code Copied

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,35 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [darkTheme, setDarkTheme] = useState(undefined);
+
+  const handleToggle = event => {
+    setDarkTheme(event.target.checked);
+  };
+
+  useEffect(() => {
+    if (darkTheme !== undefined) {
+      if (darkTheme) {
+        // Set value of  darkmode to dark
+        document.documentElement.setAttribute('data-theme', 'dark');
+        window.localStorage.setItem('theme', 'dark');
+      } else {
+        // Set value of  darkmode to light
+        document.documentElement.removeAttribute('data-theme');
+        window.localStorage.setItem('theme', 'light');
+      }
+    }
+  }, [darkTheme]);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const initialColorValue = root.style.getPropertyValue(
+      '--initial-color-mode'
+    );
+    // Set initial darkmode to light
+    setDarkTheme(initialColorValue === 'dark');
+  }, []);
+
   return (
     <>
       <div className='container'>
