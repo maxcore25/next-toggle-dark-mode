73b83f9213483a8ae39c9d2a41515ff12dc8b430
maxcore25 <mkorobov2002@mail.ru>
2022-03-09 15:07:23 +0300

    Main Components Added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div className='container'>
+        <nav>
+          <div className='mybrand'>My Personal Site</div>
+          <div>
+            {darkTheme !== undefined && (
+              <form action='#'>
+                <label className='switch'>
+                  <input
+                    type='checkbox'
+                    checked={darkTheme}
+                    onChange={handleToggle}
+                  />
+                  <span className='slider'></span>
+                </label>
+              </form>
+            )}
+          </div>
+        </nav>
+        <section>
+          <div className='heroSection'>
+            <h1>👋 Hello World</h1>
+            <h3>I am a web developer</h3>
+            <p>
+              You can find me on
+              <span>
+                <a
+                  href='https://github.com/maxcore25'
+                  target='_blank'
+                  rel='noreferrer'>
+                  GitGub
+                </a>
+              </span>
+              Lorem ipsum dolor sit amet
+            </p>
+            <button className='primary-btn'>Connect with me</button>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
